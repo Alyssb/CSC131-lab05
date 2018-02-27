2928ec017741470db559f3dab79a4e0a172c5562
Alyssb <ajs41@missouristate.edu>
2018-02-27 13:13:26 -0600

    LAB05-DEVELOP - Circle finished, initial triangle and triangle area

--- csc131/Triangle.py
+++ csc131/Triangle.py
@@ -7,5 +7,17 @@ File: Triangle.py
 Author: Alyssa Slayton <ajs41@missouristate.edu>
 """
 from csc131.GeometricObject import GeometricObject
+from math import sqrt
 
 # TODO: Replace this TODO with the Triangle definition prescribed in the README.
+class Triangle(GeometricObject):
+
+    def __init__(self, side1: float = 1.0, side2: float = 1.0, side3: float = 1.0, color: str = "white", filled: bool=True):
+        super().__init__(color,filled)
+        self._side1 = side1
+        self._side2 = side2
+        self._side3 = side3
+
+    def get_area(self) -> float:
+        s = (self._side1 + self._side2 + self._side3)
+        return sqrt(s(s - self._side1)(s - self._side2)(s - self._side3))
